9eae984d22c31c7c5f6575187920b1bf650ce9eb
imrulkayes75 <ikbubt@gmail.com>
2024-04-14 16:29:11 +0600

    MyCreate route created and Navbar added

---
+++ src/MyCreateRoute/MyCreateRoute.jsx
@@ -0,0 +1,20 @@
+import {
+    createBrowserRouter
+} from "react-router-dom";
+import MainLayOut from "../MainLayOut/MainLayOut";
+import Home from "../Components/Home/Home";
+
+const MyCreateRoute = createBrowserRouter([
+    {
+        path: '/',
+        element: <MainLayOut></MainLayOut>,
+        children: [
+            {
+                path: '/',
+                element: <Home></Home>
+            }
+        ]
+    }
+
+])
+export default MyCreateRoute;
